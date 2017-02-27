449b60fc0a8151e5288a5be118ac49153ab07b3d
Adam Donahue <adam.donahue@loan-street.com>
2017-02-27 10:03:46 -0500

    Add initial set of files for lesson one.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+
+  entry: {
+    lesson1: './lesson1/index.js',
+  },
+
+  output: {
+    path: path.resolve('./bundles'),
+    filename: '[name].js'
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+        // As long as the vendor module exists and is used once, include it.
+        return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
